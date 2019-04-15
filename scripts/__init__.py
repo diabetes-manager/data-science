8f24e2da747a05d511c8184870dfab9c9b81d769
Chris Luedtke <chrisluedtke@gmail.com>
2019-04-15 08:58:11 -0500

    fix load_cgm script and revert format edits

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,9 +2,6 @@ import re
 from pathlib import Path
 
 import pandas as pd
-# from dotenv import load_dotenv
-
-# load_dotenv()
 
 DATA_DIR = Path(__file__).parents[1] / 'data'
 
@@ -16,11 +13,8 @@ def load_uci():
     for p in Path(data_path).iterdir():
         match = re.search(r'\d\d$', str(p))
         if match:
-            df = pd.read_csv(
-                p,
-                sep='\t',
-                header=None,
-                names=['Date', 'Time', 'Code', 'Value'])
+            df = pd.read_csv(p, sep='\t', header=None, 
+                             names=['Date', 'Time', 'Code', 'Value'])
             df['person_id'] = int(match[0])
             dfs.append(df)
     df = pd.concat(dfs)
@@ -49,19 +43,17 @@ def load_uci():
     }
 
     df['code_cat'] = df['Code'].map(code_map)
-
-    df['date_time'] = pd.to_datetime(
-        df['Date'] + ' ' + df['Time'],
-        format="%m-%d-%Y %H:%M",
-        errors='coerce')
-
+    
+    df['date_time'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], 
+                                     format="%m-%d-%Y %H:%M", errors='coerce')
+    
     df = df.drop(columns=['Date', 'Time'])
-
+    
     print(f"{sum(df['date_time'].isna())} records failed to convert to date")
     df = df.loc[~df['date_time'].isna()]
-
+    
     # drop entries entered at 8AM, 12AM, 4PM, 10PM
-    mask = (df['date_time'].dt.hour.isin([8, 12, 18, 22]) &
+    mask = (df['date_time'].dt.hour.isin([8, 12, 18, 22]) & 
             (df['date_time'].dt.minute == 0))
     df = df.loc[~mask]
 
@@ -79,27 +71,28 @@ def load_tidepool_dummy():
 
 
 def load_so_pump_raw():
-    data_path = str(DATA_DIR / 'private' / 'omnipod_pump' /
+    data_path = str(DATA_DIR / 'private' / 'omnipod_pump' / 
                     'omnipod_export_2019-04-13.TAB')
     df = pd.read_csv(data_path, sep='\t', encoding='latin1')
 
-    cols = [
-        'DATEEVENT', 'TIMESLOT', 'EVENTTYPE', 'VENDOR_EVENT_ID', 'KEY0',
-        'KEY1', 'KEY2', 'I0', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8',
-        'I9', 'D0', 'D1', 'D2', 'D3', 'D4', 'C0', 'C1', 'C2', 'COMMENT'
-    ]
+    cols = ['DATEEVENT', 'TIMESLOT', 'EVENTTYPE', 'VENDOR_EVENT_ID', 
+            'KEY0', 'KEY1', 'KEY2', 
+            'I0', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 
+            'D0', 'D1', 'D2', 'D3', 'D4', 
+            'C0', 'C1', 'C2', 'COMMENT']
     df = df[cols]
 
     df['DATEEVENT'] = pd.to_datetime(
-        pd.to_numeric(df['DATEEVENT']) - 2, origin='1900-01-01',
-        unit="D").dt.round('s')
+        pd.to_numeric(df['DATEEVENT']) - 2,
+        origin='1900-01-01', unit="D"
+    ).dt.round('s')
 
     return df.sort_values('DATEEVENT').reset_index(drop=True)
 
 
 def load_so_pump_clean():
     """Data pulled by copy & paste from Abbott desktop app"""
-    data_path = str(DATA_DIR / 'private' / 'omnipod_pump' /
+    data_path = str(DATA_DIR / 'private' / 'omnipod_pump' / 
                     'omnipod_export_2019-04-13.csv')
     df = pd.read_csv(data_path)
 
@@ -107,16 +100,13 @@ def load_so_pump_clean():
     df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
     df = df.drop(columns=['Date', 'Time'])
 
-    df[['Value', 'Unit',
-        '_']] = (df['Value'].str.replace(r'\(|\)', '').str.split(
-            ' ', expand=True))
+    df[['Value', 'Unit', '_']] = (df['Value'].str.replace(r'\(|\)', '')
+                                             .str.split(' ', expand=True))
     df = df.drop(columns=['_'])
     df['Value'] = pd.to_numeric(df['Value'])
 
-    cols = [
-        'Datetime', 'Type', 'Value', 'Unit', 'Description', 'Other Info',
-        'Comment'
-    ]
+    cols = ['Datetime', 'Type', 'Value', 'Unit', 'Description', 
+            'Other Info', 'Comment']
 
     return df[cols]
 
@@ -128,14 +118,14 @@ def load_so_cgm():
     for p in Path(data_path).iterdir():
         if str(p).endswith('.csv'):
             df = pd.read_csv(p)
-
+            
             date_nans = df['Timestamp (YYYY-MM-DDThh:mm:ss)'].isna()
             n_date_nans = sum(date_nans)
             if n_date_nans > 9:
                 raise Exception(f'More NaN Timestamp values than expected. '
                                 f'Expected <= 9, found {n_date_nans}')
-
             df = df.loc[~date_nans]
+
             dfs.append(df)
 
-    return df
+    return pd.concat(dfs).sort_values('Timestamp (YYYY-MM-DDThh:mm:ss)')
