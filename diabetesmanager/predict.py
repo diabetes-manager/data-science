9e36608ec65ee428e7a35a55a7117ac853e6f37c
Chris Luedtke <chrisluedtke@gmail.com>
2019-04-19 09:22:45 -0500

    add type checking

--- diabetesmanager/predict.py
+++ diabetesmanager/predict.py
@@ -6,23 +6,22 @@ import pandas as pd
 import sklearn
 
 
-def preprocess(df, minutes=30, n_historical_cols=2):
-    # TODO: type checking
-    # timestamp should be int
-    # measure should be int
+def type_check(df):
+    for col in ['timestamp', 'value', 'below_threshold']:
+        assert pd.api.types.is_numeric_dtype(df[col])
+    
+    return None
 
-    # convert datetime to into - shouldn't be necessary in production
-    df['timestamp'] = df['timestamp'].astype(np.int64) // 10**9
 
-    for x in range(1, n_historical_cols + 1):
-        df[['prev_meas', 'prev_time']] = (df[['value', 'timestamp']].shift(x))
+def feature_engineer(df, minutes=30, n_historical_cols=2):
+    """Adds most recent CGM rates as columns"""
+    type_check(df)
+    
+    for x in range(1, n_historical_cols+1):
+        df[['prev_val', 'prev_time']] = df[['value', 'timestamp']].shift(x)
         df[f'prev_trend_{x}'] = (
-            df['prev_meas'].divide(df['timestamp'] - df['prev_time']))
-        df = df.drop(columns=['prev_meas', 'prev_time'])
-
-    # TODO: Other features?
-    # * hour of day
-    # * day of week
+            df['prev_val'].divide(df['timestamp'] - df['prev_time']))
+        df = df.drop(columns=['prev_val', 'prev_time'])
 
     # remove nans
     og_len = len(df)
@@ -34,11 +33,11 @@ def preprocess(df, minutes=30, n_historical_cols=2):
 
 
 def make_prediction(user_df, model, minutes=30):
-    df = preprocess(user_df, minutes)
+    df = feature_engineer(user_df, minutes)
     df = df.iloc[-1:]
-    predictions = model.predict(df)
+    prediction = model.predict(df)
 
     return pd.DataFrame({
         'timestamp': [df['timestamp'].max() + 60 * minutes],
-        'predicted_value': [int(predictions[0].round())]
+        'predicted_value': [int(prediction[0].round())]
     })
